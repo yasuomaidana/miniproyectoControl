231d5c168b6e3c7ff2df05f9694ff51d97279422
Yasuo Maidana <a01328427@itesm.mx>
2020-10-25 14:23:46 -0600

    acompletando menu

--- menu.m
+++ menu.m
@@ -1,14 +1,26 @@
 clear
 clc
 close all
+op=0;
 while ~xor(op~=1,op~=2)
     op=input('Desea : \n    simular el carro(1) \n    cargar las graficas de una simulacion previa(2) \n Opcion :');
     if ~xor(op~=1,op~=2)
         disp('Seleccione una opcion valida')
     end
 end
+
 switch op
     case 1
-        
+        deadProofController();
     case 2
+        
+        bezierPlotter('camino.csv')
+        hold on
+        load('trayectoria','basepos')
+        x=basepos(:,1);
+        y=basepos(:,2);
+        plot(x,y,'r')
+        legend('Camino propuesto','Trayectoria reccorida')
+        title('Camino propuesto vs recorrido')
+        basepos=[x,y];
 end
